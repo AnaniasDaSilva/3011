e1a259ce12727eb81b1a6d2ed2e13767006dde43
teste_git <113646572+AnaniasDaSilva@users.noreply.github.com>
2022-12-02 11:06:48 -0300

    ccs_atualizado

--- netflix.php
+++ netflix.php
@@ -13,20 +13,7 @@ if(isset($_GET['num1'],$_GET['num2'],$_GET['calcular'])){
     $num2 = $_GET['num2'];
     $calcular = $_GET['calcular'];
 
-    switch($calcular){
-        case 'drama';
-            $resultado = $num1 + $num2;
-            break;
-        case 'terror';
-            $resultado = $num1 - $num2;
-            break;    
-        case 'romnce';
-            $resultado = $num1 * $num2;
-            break;
-        case 'ação';
-            $resultado = $num1 / $num2;
-            break;
-    }
+   
 }
 
 
@@ -39,28 +26,33 @@ if(isset($_GET['num1'],$_GET['num2'],$_GET['calcular'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soma entre dois números</title>
+    <title>Netflix</title>
 </head>
 <style>
     .form{
         border-radius: 30px;
-        background-color: #5CAAE6;
-        margin-left: 350px;
-        margin-right: 350px;
+        background-color: black;
+        margin-top:10px;
+        margin-left: 600px;
+        margin-right: 500px;
+        height:600px;
         text-align: center;
         justify-content: center;
         align-items:center;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         padding: 30px;
+        opacity:0.5;
         color: #DAF2E3;
     }
 
     .inputs {
-        padding: 10px;
+        padding: 20px;
         border-radius: 10px;
     }
     .body{
         background-image: url("./filme nada de novo.jpg");
+        background-position:cover;
+       
     }
 
 </style>
@@ -70,10 +62,10 @@ if(isset($_GET['num1'],$_GET['num2'],$_GET['calcular'])){
     <h3>Entrar</h3>
         
     <form>
-        <input type="text" id="email ou num telefone" class="inputs" name="email ou num telefone" required/>
+        <input placeholder="E-mail ou telefone"type="text" id="email ou num telefone" class="inputs" name="email ou num telefone" required/>
 <br/>
 <br/>
-        <input type="number" id="num1" class="inputs" name="num2" required/>
+        <input placeholder="senha" type="number" id="num1" class="inputs" name="num2" required/>
         <br/>
 <br/>
        <br />
